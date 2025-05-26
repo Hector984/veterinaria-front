8f4bd6443cb7768f216cdf976446946c214dc340
Hector984 <hectoruam96@gmail.com>
2025-05-26 08:26:13 -0600

    Diseño de pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import './App.css'
+import logo from './assets/logo-honda.png';
+import honda from './assets/honda.jpg';
+
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <div className="h-screen flex items-center flex-col">
+        <img className='rounded-lg' src={logo} alt="Logo Honda" />
+        <p className='uppercase mb-6 font-mono text-2xl'>pilot 2020</p>
+        <img className='rounded-lg' src={honda} alt="Honda Pilot 2020" />
+        <div className='mt-6 px-8 w-full flex justify-around'>
+          <ul className='leading-8 w-1/2 px-3'>
+            <li className='break-words'>Sistema de Mitigación de Colisión con Frenado (CMBS™) + Sistema de Alerta de Frenado por Colisión Frontal (FCW)</li>
+            <li className='break-words'>Control de Velocidad Crucero Adaptativo (ACC) + Función de Seguimiento a Baja Velocidad (Low Speed Follow)</li>
+            <li>Sistema Automático de Luces Altas (AHB)</li>
+            <li>Sistema de Prevención de Colisión Trasera (CTM)</li>
+          </ul>
+          <ul className='leading-8 w-1/2 px-3'>
+            <li>Sistema de Conservación de Carril (LKAS) + Alerta de cambio de carril (LDW)</li>
+            <li>Sistema de Mitigación de Salida de Carretera (RDMS)</li>
+            <li>Asistencia en Tráfico Pesado (TJA)</li>
+            <li>Sistema de Monitoreo de Punto Ciego (BSI)</li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
